6ee668bd703356faf8a13c02405fd6506ae8cbc2
Michael Uno <michael@michaeluno.jp>
2015-07-26 05:46:13 +0900

    Fixed code formatting for readability.

--- include/class/utility/task/TaskScheduler_TaskUtility_Add.php
+++ include/class/utility/task/TaskScheduler_TaskUtility_Add.php
@@ -2,9 +2,9 @@
 /**
  * One of the abstract parent classes of the TaskScheduler_TaskUtility class.
  * 
- * @package     Task Scheduler
- * @copyright   Copyright (c) 2014, Michael Uno
- * @author        Michael Uno
+ * @package      Task Scheduler
+ * @copyright    Copyright (c) 2014-2015, Michael Uno
+ * @author       Michael Uno
  * @authorurl    http://michaeluno.jp
  * @since        1.0.0
  */
@@ -18,25 +18,25 @@ abstract class TaskScheduler_TaskUtility_Add extends TaskScheduler_TaskUtility_G
     static public $aDefaultMeta = array(
     
         // Required internals
-        '_routine_status'        =>    null,    // string    ready, awaiting, processing
-        '_next_run_time'         =>    null,    // float
-        '_count_call'            =>    0,        // integer - represents the count that the task is called (triggered)
-        '_count_exit'            =>    0,        // integer - represents the count that the task returned an exit code (indicated completion).
-        '_count_run'             =>    0,        // integer - represents the count that the task has been executed. (which does not mean the task did what the user expects but it did finish running)
-        '_count_hung'            =>    0,        // integer - represents the count that the task got hung.
+        '_routine_status'        => null,    // string    ready, awaiting, processing
+        '_next_run_time'         => null,    // float
+        '_count_call'            => 0,       // integer - represents the count that the task is called (triggered)
+        '_count_exit'            => 0,       // integer - represents the count that the task returned an exit code (indicated completion).
+        '_count_run'             => 0,       // integer - represents the count that the task has been executed. (which does not mean the task did what the user expects but it did finish running)
+        '_count_hung'            => 0,       // integer - represents the count that the task got hung.
         
         // Required
-        'routine_action'         =>    null,    // string    the target action hook name
-        'argument'               =>    null,    // array    the arguments(parameters) passed to the action
-        'occurrence'             =>    null,    // string    The slug of the occurrence type.
+        'routine_action'         => null,    // string    the target action hook name
+        'argument'               => null,    // array    the arguments(parameters) passed to the action
+        'occurrence'             => null,    // string    The slug of the occurrence type.
         
         // Not sure why the underscore is not pre-fixed <-- maybe when creating a thread, this is needed and to third party developers need to know this.
-        'log_id'                 =>    null,    // integer    the last set log id.
+        'log_id'                 => null,    // integer    the last set log id.
         
         // Advanced options - they also have a prefix of a underscore to prevent conflicts with third-party extensions.
-        '_max_root_log_count'    =>    0,
+        '_max_root_log_count'    => 0,
 // TODO: assign a server set PHP max execution time here.
-        '_max_execution_time'    =>    null,    // integer    whenever retrieve this value, assign the server set maximum execution time.
+        '_max_execution_time'    => null,    // integer    whenever retrieve this value, assign the server set maximum execution time.
         
     );    
         
@@ -72,22 +72,22 @@ abstract class TaskScheduler_TaskUtility_Add extends TaskScheduler_TaskUtility_G
                 '_count_call'       => 0,
                 '_count_exit'       => 0,
                 '_count_run'        => 0,
-                // 'post_parent'       =>    $iParentTaskID,    //<-- this used to be always 0 but we are going to allow child tasks in near future.
+                // 'post_parent'    =>    $iParentTaskID,    //<-- this used to be always 0 but we are going to allow child tasks in near future.
             ),
             $aTaskOptions    // since the method is performed recursively, but the 'tax_input' should not be recursively merged, use the union operator. 
             + array(
-                'post_status'          => 'private',
-                '_routine_status'      => 'ready',
-                'tax_input'            => array( 
+                'post_status'       => 'private',
+                '_routine_status'   => 'ready',
+                'tax_input'         => array( 
                     TaskScheduler_Registry::$aTaxonomies[ 'system' ] => $aSystemTaxonomyTerms
                 ),    
             ) 
             + self::$aDefaultMeta
         );
         unset( 
-            $aTaskOptions['_is_spawned'],          // if this is set, the action will not be loaded
-            $aTaskOptions['_last_run_time'],       // a new task should not have a last executed time
-            $aTaskOptions['_exit_code']            // a new task should not have an exit code
+            $aTaskOptions[ '_is_spawned' ],          // if this is set, the action will not be loaded
+            $aTaskOptions[ '_last_run_time' ],       // a new task should not have a last executed time
+            $aTaskOptions[ '_exit_code' ]            // a new task should not have an exit code
         );
         
         // This allows a custom post type to be passed.
