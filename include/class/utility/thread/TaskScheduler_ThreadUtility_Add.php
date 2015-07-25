0ab34f79747e879dc2066d886f4271f910ebab62
Michael Uno <michael@michaeluno.jp>
2015-07-26 05:42:40 +0900

    - Fixed code formatting for readability.

--- include/class/utility/thread/TaskScheduler_ThreadUtility_Add.php
+++ include/class/utility/thread/TaskScheduler_ThreadUtility_Add.php
@@ -2,9 +2,9 @@
 /**
  * One of the abstract parent classes of the TaskScheduler_ThreadUtility class.
  * 
- * @package     Task Scheduler
- * @copyright   Copyright (c) 2014, Michael Uno
- * @author        Michael Uno
+ * @package      Task Scheduler
+ * @copyright    Copyright (c) 2014-2015, Michael Uno
+ * @author       Michael Uno
  * @authorurl    http://michaeluno.jp
  * @since        1.0.0
  */
@@ -15,24 +15,24 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
      * Represents the default meta key-values of the thread post type posts.
      */
     static public $aDefaultMeta = array(
-        '_routine_status'         =>    'queued',    // string    ready, awaiting, processing
-        '_next_run_time'          =>    null,        // float
-        '_count_call'           =>    0,        // integer - represents the count that the task is called (triggered)
-        '_count_run'            =>    0,        // integer - represents the count that the task has been executed. (which does not mean the task did what the user expects but it did finish running)        
-        'routine_action'          =>    null,        // string    the target action hook name
-        // 'argument'             =>    array(),    // array    the arguments(parameters) passed to the action
-        'occurrence'              =>    'volatile',    // string    The slug of the occurrence type. At the moment, only the 'volatile' occurrence type is allowed.
-        'parent_routine_log_id'   =>    null,
-        'owner_routine_id'        =>    null,
-        // 'log_id'               =>    null,        // integer    the last set log id.    
-        '_max_execution_time'     =>    null,        // integer    This will be inherited from the owner task's value.
+        '_routine_status'         => 'queued',    // string    ready, awaiting, processing
+        '_next_run_time'          => null,        // float
+        '_count_call'             => 0,           // integer - represents the count that the task is called (triggered)
+        '_count_run'              => 0,           // integer - represents the count that the task has been executed. (which does not mean the task did what the user expects but it did finish running)        
+        'routine_action'          => null,        // string    the target action hook name
+        // 'argument'             => array(),     // array    the arguments(parameters) passed to the action
+        'occurrence'              => 'volatile',  // string    The slug of the occurrence type. At the moment, only the 'volatile' occurrence type is allowed.
+        'parent_routine_log_id'   => null,
+        'owner_routine_id'        => null,
+        // 'log_id'               => null,        // integer    the last set log id.    
+        '_max_execution_time'     => null,        // integer    This will be inherited from the owner task's value.
     );            
 
     
     /**
      * Derives a thread from the task given task with the given options.
      * 
-     * The difference with the add() method is that it can be canceled if the same thread already exists.
+     * The difference with the add() method is that it can be cancelled if the same thread already exists.
      * This is used to create an internal thread that should not add a new one if the same thread already exists.
      * Note that it does not check taxonomy terms. Only the post title and the meta keys-values.
      * 
@@ -41,14 +41,18 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
      */
     static public function derive( $iOwnerRoutineID, array $aThreadOptions, array $aSystemTaxonomyTerms=array(), $bAllowDuplicate=false ) {
         
-        if ( ! self::doesPostExist( $iOwnerRoutineID ) ) { return 0; }
+        if ( ! self::doesPostExist( $iOwnerRoutineID ) ) { 
+            return 0; 
+        }
         
         if ( ! $bAllowDuplicate && self::hasSameRoutine( $aThreadOptions, array( __CLASS__, 'find' ) ) ) {
             return 0;
         }
         
         $_oOwnerRoutine = TaskScheduler_Routine::getInstance( $iOwnerRoutineID );
-        if ( ! is_object( $_oOwnerRoutine ) ) { return 0; }        
+        if ( ! is_object( $_oOwnerRoutine ) ) { 
+            return 0;
+        }
          
         $_aOwnerTerms = wp_get_post_terms( 
             $iOwnerRoutineID,
@@ -56,7 +60,7 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             array( "fields" => "names" ) 
         );              
 
-        return self::add( $iOwnerRoutineID, $aThreadOptions, array_merge( $_aOwnerTerms, $aSystemTaxonomyTerms ) );       
+        return self::add( $iOwnerRoutineID, $aThreadOptions, array_merge( $_aOwnerTerms, $aSystemTaxonomyTerms ) );
         
     }
         /**
@@ -72,28 +76,28 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             );
             // The following change dynamically so don't check
             $_aNotCheckingTopLevelArguments = array(
-                'ID', // [ <post id> ] // Are you updating an existing post?
-                'tax_input',    // => [ array( <taxonomy> => <array | string> ) ] // For custom taxonomies. Default empty.
-                'post_type',    //     => [ 'post' | 'page' | 'link' | 'nav_menu_item' | custom post type ] // Default 'post'.
-                'post_author',    //    => [ <user ID> ] // The user ID number of the author. Default is the current user ID.
-                'post_status',    //   => [ 'draft' | 'publish' | 'pending'| 'future' | 'private' | custom registered status ] // Default 'draft'.
-                'post_content'   , // [ <string> ] // The full text of the post.
-                'post_name'      , // [ <string> ] // The name (slug) for your post            
-                'post_parent'    , // [ <post ID> ] // Sets the parent of the new post, if any. Default 0.
-                'menu_order'     , // [ <order> ] // If new post is a page, sets the order in which it should appear in supported menus. Default 0.
-                'ping_status',    //    => [ 'closed' | 'open' ] // Pingbacks or trackbacks allowed. Default is the option 'default_ping_status'.
-                'to_ping'        , // // Space or carriage return-separated list of URLs to ping. Default empty string.
-                'pinged'         , // // Space or carriage return-separated list of URLs that have been pinged. Default empty string.
-                'post_password'  , // [ <string> ] // Password for post, if any. Default empty string.
-                'guid'           , // // Skip this and let Wordpress handle it, usually.
-                'post_content_filtered' , // // Skip this and let Wordpress handle it, usually.
-                'post_excerpt'   , // [ <string> ] // For all your post excerpt needs.
-                'post_date'      , // [ Y-m-d H:i:s ] // The time post was made.
-                'post_date_gmt'  , // [ Y-m-d H:i:s ] // The time post was made, in GMT.
-                'comment_status' , // [ 'closed' | 'open' ] // Default is the option 'default_comment_status', or 'closed'.
-                'post_category'  , // [ array(<category id>, ...) ] // Default empty.
-                'tags_input'     , // [ '<tag>, <tag>, ...' | array ] // Default empty.            
-                'page_template'  , // [ <string> ] // Requires name of template file, eg template.php. Default empty.
+                'ID',               // [ <post id> ] // Are you updating an existing post?
+                'tax_input',        // => [ array( <taxonomy> => <array | string> ) ] // For custom taxonomies. Default empty.
+                'post_type',        // => [ 'post' | 'page' | 'link' | 'nav_menu_item' | custom post type ] // Default 'post'.
+                'post_author',      // => [ <user ID> ] // The user ID number of the author. Default is the current user ID.
+                'post_status',      // => [ 'draft' | 'publish' | 'pending'| 'future' | 'private' | custom registered status ] // Default 'draft'.
+                'post_content',     // [ <string> ] // The full text of the post.
+                'post_name',        // [ <string> ] // The name (slug) for your post            
+                'post_parent',      // [ <post ID> ] // Sets the parent of the new post, if any. Default 0.
+                'menu_order',       // [ <order> ] // If new post is a page, sets the order in which it should appear in supported menus. Default 0.
+                'ping_status',      //    => [ 'closed' | 'open' ] // Pingbacks or trackbacks allowed. Default is the option 'default_ping_status'.
+                'to_ping',          // Space or carriage return-separated list of URLs to ping. Default empty string.
+                'pinged',           // Space or carriage return-separated list of URLs that have been pinged. Default empty string.
+                'post_password',    // [ <string> ] // Password for post, if any. Default empty string.
+                'guid',             // Skip this and let Wordpress handle it, usually.
+                'post_content_filtered', // // Skip this and let Wordpress handle it, usually.
+                'post_excerpt',     // [ <string> ] // For all your post excerpt needs.
+                'post_date',        // [ Y-m-d H:i:s ] // The time post was made.
+                'post_date_gmt',    // [ Y-m-d H:i:s ] // The time post was made, in GMT.
+                'comment_status',   // [ 'closed' | 'open' ] // Default is the option 'default_comment_status', or 'closed'.
+                'post_category',    // [ array(<category id>, ...) ] // Default empty.
+                'tags_input',       // [ '<tag>, <tag>, ...' | array ] // Default empty.            
+                'page_template',    // [ <string> ] // Requires name of template file, eg template.php. Default empty.
             );
             // Extract the top level argument elements and remove them from the meta array.
             $_aQueryArguments = array();
@@ -116,20 +120,22 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             }
                     
             // Compose the 'meta_query' argument element.
-            $_aMetaQuery = array( 'relation' =>    'AND' );
+            $_aMetaQuery = array( 'relation' => 'AND' );
             foreach ( $_aThreadMeta as $_sKey => $_vValue ) {
                 $_aMetaQuery[] = array(
-                    'key'        =>    $_sKey,
-                    'value'        =>    $_vValue,
-                    'compare'    =>    'IN',                
+                    'key'        => $_sKey,
+                    'value'      => $_vValue,
+                    'compare'    => 'IN',                
                 );
             }
-            $_aQueryArguments[ 'meta_query' ]        = $_aMetaQuery;
-            $_aQueryArguments[ 'post_status ' ]        = 'any';
-            $_aQueryArguments[ 'posts_per_page' ]    = 1;
+            $_aQueryArguments[ 'meta_query' ]     = $_aMetaQuery;
+            $_aQueryArguments[ 'post_status ' ]   = 'any';
+            $_aQueryArguments[ 'posts_per_page' ] = 1;
              
             $_aResults = self::find( $_aQueryArguments );
-            return $_aResults->found_posts ? true : false;
+            return $_aResults->found_posts 
+                ? true 
+                : false;
             
         }    
     
@@ -143,7 +149,9 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
     static public function add( $iOwnerRoutineID, array $aThreadOptions=array(), array $aSystemTaxonomyTerms=array() ) {
         
         $_oOwnerTask = TaskScheduler_Routine::getInstance( $iOwnerRoutineID );
-        if ( ! is_object( $_oOwnerTask ) ) { return 0; }
+        if ( ! is_object( $_oOwnerTask ) ) { 
+            return 0; 
+        }
     
         $aThreadOptions = array(
                 // thread specific keys
@@ -156,7 +164,7 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             + $aThreadOptions
             + array(
                 'post_status'           => 'private',
-                '_next_run_time'        => microtime( true ),    // queue now
+                '_next_run_time'        => microtime( true ), // queue now
                 'tax_input'             => array( 
                     TaskScheduler_Registry::$aTaxonomies[ 'system' ] => $aSystemTaxonomyTerms
                 ),        
@@ -165,11 +173,11 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             + self::$aDefaultMeta;
             
         unset( 
-            $aThreadOptions['_is_spawned'],         // if this is set, the action will not be loaded
-            $aThreadOptions['_last_run_time'],        
-            $aThreadOptions['_exit_code'],            // a new task should not have an exit code
-            $aThreadOptions['_count_exit'],            
-            $aThreadOptions['_count_hung']    
+            $aThreadOptions[ '_is_spawned' ],    // if this is set, the action will not be loaded
+            $aThreadOptions[ '_last_run_time' ],        
+            $aThreadOptions[ '_exit_code' ],     // a new task should not have an exit code
+            $aThreadOptions[ '_count_exit' ],            
+            $aThreadOptions[ '_count_hung' ]    
         );        
         
         // Currently only the 'volatile' occurrence type is allowed.
@@ -178,7 +186,10 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
         }
 
         // Create a post.
-        $_iThreadID = self::insertPost( $aThreadOptions, TaskScheduler_Registry::$aPostTypes[ 'thread' ] );
+        $_iThreadID = self::insertPost( 
+            $aThreadOptions, 
+            TaskScheduler_Registry::$aPostTypes[ 'thread' ]
+        );
                                                 
         // Add terms because the 'tax_input' argument does not take effect for some reasons when multiple terms are set.
          $_aSystemInternalTerms = isset( $aThreadOptions['tax_input'][ TaskScheduler_Registry::$aTaxonomies[ 'system' ] ] )
@@ -186,7 +197,12 @@ abstract class TaskScheduler_ThreadUtility_Add extends TaskScheduler_ThreadUtili
             : array();
 
         if ( ! empty( $_aSystemInternalTerms ) ) {
-            wp_set_object_terms( $_iThreadID, $_aSystemInternalTerms, TaskScheduler_Registry::$aTaxonomies[ 'system' ], true );
+            wp_set_object_terms( 
+                $_iThreadID, 
+                $_aSystemInternalTerms, 
+                TaskScheduler_Registry::$aTaxonomies[ 'system' ], 
+                true 
+            );
         }                
                 
         return $_iThreadID;
