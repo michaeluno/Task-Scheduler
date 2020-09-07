52f95ec280751bbe36388e5fac7d274328cfc1a6
Michael Uno <michael2@michaeluno.jp>
2020-09-07 20:57:50 +0900

    Cleaned code.

--- include/class/utility/thread/TaskScheduler_ThreadUtility_Get.php
+++ include/class/utility/thread/TaskScheduler_ThreadUtility_Get.php
@@ -14,29 +14,20 @@ abstract class TaskScheduler_ThreadUtility_Get extends TaskScheduler_ThreadUtili
     /**
      * Retrieves tasks with the given criteria.
      * 
-     * @param    array|string    $asStatuses    pass the task status from the followings:
-     * - queued
-     * - started
-     * - completed
-     * - lost
-     * 
-     * @param    boolean        $bInternal        Indicates whether or not the retrieving tasks are all internal.
-     * @return    object            The WP Query return object holding the result. To retrieve the post ids call the 'posts' element like $oResult->posts.
+     * @param    array $aArgs   Overriding query arguments.
+     * @return   WP_Query       A WP Query object holding the result. To retrieve the post ids call the 'posts' element like $oResult->posts.
      */
     static public function find( array $aArgs=array() ) {
-                
-        // Construct the query argument array.
-        $_aArgs = $aArgs + array(
-            'post_type'         => TaskScheduler_Registry::$aPostTypes[ 'thread' ],
-            'post_status'       => array( 'publish', 'private' ),
-            'posts_per_page'    => -1,    // -1 for all            
-            'orderby'           => 'date ID',        // another option: 'ID',    
-            'order'             => 'DESC', // DESC: the newest comes first, 'ASC' : the oldest comes first
-            'fields'            => 'ids',    // return only post IDs
+        return new WP_Query(
+            $aArgs + array(
+                'post_type'         => TaskScheduler_Registry::$aPostTypes[ 'thread' ],
+                'post_status'       => array( 'publish', 'private' ),
+                'posts_per_page'    => -1,    // -1 for all
+                'orderby'           => 'date ID',        // another option: 'ID',
+                'order'             => 'DESC', // DESC: the newest comes first, 'ASC' : the oldest comes first
+                'fields'            => 'ids',    // return only post IDs
+            )
         );
-
-        return new WP_Query( $_aArgs );
-        
     }        
         
     /**
